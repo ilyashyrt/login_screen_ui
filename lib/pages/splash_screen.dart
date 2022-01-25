c4eb6e9dd49e7960301930bc2bf296feb276c968
DESKTOP-PBREBOD\ILYAS <ihayrat786@gmail.com>
2022-01-25 21:22:12 +0300

    arrangements have been made

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:login_ekrani/utils/constants.dart';
 import 'package:login_ekrani/utils/device_utils.dart';
 
@@ -31,20 +32,56 @@ class _SplashScreenState extends State<SplashScreen> {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-              color: Colors.white,
+              color: Constants.scaffoldBackgroundColor,
             ),
             width: DeviceUtils.getScaledWidth(context, 1),
             height: DeviceUtils.getScaledHeight(context, 0.4),
             child: Container(
               child: Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(20.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Welcome to Laundree!",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                    Text("This is the first version of our laundry app. Please sign in or create an account below"),
-                    //ElevatedButton(onPressed: (){}, child: Text("Log In",style: TextStyle(color: Constants.greenColor),)),
+                    Text(
+                      "Welcome to Laundree!",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "This is the first version of our laundry app. Please sign in or create an account below",
+                        style: GoogleFonts.poppins(
+                            textStyle:
+                                TextStyle(color: Colors.black.withOpacity(0.6))),
+                      ),
+                    ),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          primary: Constants.scaffoldBackgroundColor,
+                          fixedSize: Size(DeviceUtils.getScaledWidth(context, 1),50),
+                          elevation: 5,
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Log In",
+                          style: GoogleFonts.poppins(textStyle: TextStyle(color: Constants.greenColor,fontSize: 16,fontWeight: FontWeight.bold)),
+                        )),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          primary: Constants.greenColor,
+                          fixedSize: Size(DeviceUtils.getScaledWidth(context, 1),50),
+                          elevation: 0,
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Create an Account",
+                          style: GoogleFonts.poppins(textStyle: TextStyle(color: Constants.scaffoldBackgroundColor,fontSize: 16,fontWeight: FontWeight.bold)),
+                        )),
                   ],
                 ),
               ),
