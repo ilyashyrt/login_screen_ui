83e45d0c3b132cfc667b788b5d868700da3e130e
DESKTOP-PBREBOD\ILYAS <ihayrat786@gmail.com>
2022-01-31 22:48:50 +0300

    stack structure and new assets added

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -17,78 +17,97 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Constants.greenColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: Image.asset(
-              "assets/machine.png",
-              width: DeviceUtils.getScaledWidth(context, 0.5),
+      body: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 40),
+          child: Image.asset("assets/cloth_faded.png",width: DeviceUtils.getScaledWidth(context, 1)),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Image.asset(
+                "assets/machine.png",
+                width: DeviceUtils.getScaledWidth(context, 0.5),
+                height: DeviceUtils.getScaledHeight(context, 0.4),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)),
+                color: Constants.scaffoldBackgroundColor,
+              ),
+              width: DeviceUtils.getScaledWidth(context, 1),
               height: DeviceUtils.getScaledHeight(context, 0.4),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-              color: Constants.scaffoldBackgroundColor,
-            ),
-            width: DeviceUtils.getScaledWidth(context, 1),
-            height: DeviceUtils.getScaledHeight(context, 0.4),
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Welcome to Laundree!",
-                      style: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: Text(
-                        "This is the first version of our laundry app. Please sign in or create an account below",
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome to Laundree!",
                         style: GoogleFonts.poppins(
-                            textStyle:
-                                TextStyle(color: Colors.black.withOpacity(0.6))),
+                            textStyle: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20)),
                       ),
-                    ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                          primary: Constants.scaffoldBackgroundColor,
-                          fixedSize: Size(DeviceUtils.getScaledWidth(context, 1),50),
-                          elevation: 5,
-                        ),
-                        onPressed: () {},
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
                         child: Text(
-                          "Log In",
-                          style: GoogleFonts.poppins(textStyle: TextStyle(color: Constants.greenColor,fontSize: 16,fontWeight: FontWeight.bold)),
-                        )),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                          primary: Constants.greenColor,
-                          fixedSize: Size(DeviceUtils.getScaledWidth(context, 1),50),
-                          elevation: 0,
+                          "This is the first version of our laundry app. Please sign in or create an account below",
+                          style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                  color: Colors.black.withOpacity(0.6))),
                         ),
-                        onPressed: () {},
-                        child: Text(
-                          "Create an Account",
-                          style: GoogleFonts.poppins(textStyle: TextStyle(color: Constants.scaffoldBackgroundColor,fontSize: 16,fontWeight: FontWeight.bold)),
-                        )),
-                  ],
+                      ),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            primary: Constants.scaffoldBackgroundColor,
+                            fixedSize: Size(
+                                DeviceUtils.getScaledWidth(context, 1), 50),
+                            elevation: 5,
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "Log In",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    color: Constants.greenColor,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold)),
+                          )),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            primary: Constants.greenColor,
+                            fixedSize: Size(
+                                DeviceUtils.getScaledWidth(context, 1), 50),
+                            elevation: 0,
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "Create an Account",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    color: Constants.scaffoldBackgroundColor,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold)),
+                          )),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
+      ]),
     );
   }
 }
