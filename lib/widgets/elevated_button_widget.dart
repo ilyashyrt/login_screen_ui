20869a3b64e176fe7388f386bf1e2694b82f732c
DESKTOP-PBREBOD\ILYAS <ihayrat786@gmail.com>
2022-03-21 11:15:22 +0300

    added elevated button widget

---
+++ lib/widgets/elevated_button_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:login_ekrani/constants/colors.dart';
+import 'package:login_ekrani/constants/strings.dart';
+import 'package:login_ekrani/constants/text_styles.dart';
+import 'package:login_ekrani/utils/device_utils.dart';
+
+class ElevatedButtonWidget extends StatelessWidget {
+  final Color? backgroundColor;
+  final String buttonText;
+  final Function() onPressed;
+  final double? elevation;
+  final TextStyle? textStyle;
+  const ElevatedButtonWidget({
+    Key? key,
+    this.backgroundColor,
+    required this.buttonText,
+    required this.onPressed,
+    this.elevation,
+    this.textStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
+          primary: backgroundColor ?? AppColors.scaffoldBackgroundColor,
+          fixedSize: Size(DeviceUtils.getScaledWidth(context, 1), 50.h),
+          elevation: elevation ?? 5,
+        ),
+        onPressed: onPressed,
+        child: Text(
+          buttonText,
+          style: textStyle ?? AppTextStyles.homePageLoginButtonTextStyle,
+        ));
+  }
+}
